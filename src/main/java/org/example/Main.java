1d7b51bcf61fdde8365d103d5687652c5570843a
kseniasklarova <k.shklyarova30@mail.ru>
2025-07-02 17:36:05 +0300

    new main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Calculator calc = new Calculator();
-        int a = 100;
+        int a = 1000;
         int b = 5000;
         System.out.println(calc.add(a, b));
         System.out.println(calc.subtract(a, b));
